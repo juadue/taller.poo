26f43cc2566ca5c0aa92da76e03df7ee740d59ab
juadue <juan11-d@hotmail.com>
2022-07-02 12:26:08 -0500

    Agrego clases para Vehiculos

---
+++ src/edu/itli/talleres/poo/vehiculos/Concesionario.java
@@ -0,0 +1,13 @@
+package edu.itli.talleres.poo.vehiculos;
+
+public class Concesionario {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Hola Juan!");
+	Vehiculo miPrimerVehiculo=new Vehiculo("Mazda", "APT125", "Automovil");
+	miPrimerVehiculo.imprimirDatos();
+	}
+
+}
---
+++ src/edu/itli/talleres/poo/vehiculos/Vehiculo.java
@@ -0,0 +1,69 @@
+package edu.itli.talleres.poo.vehiculos;
+
+public class Vehiculo {
+	private String marca;
+	private String placa;
+	private String tipo;
+	
+	/**
+	 * Constructor que permite crear un objeto Vehiculo
+	 * @param marca
+	 * @param placa
+	 * @param tipo
+	 */
+	public Vehiculo(String marca, String placa, String tipo) {
+		super();
+		this.marca = marca;
+		this.placa = placa;
+		this.tipo = tipo;
+	}
+	
+	public void imprimirDatos() {
+		System.out.println("Marca: "+ marca);
+		System.out.println("Placa: "+ placa);
+		System.out.println("Tipo: "+ tipo);
+	}
+
+	/**
+	 * @return the marca
+	 */
+	public String getMarca() {
+		return marca;
+	}
+
+	/**
+	 * @param marca the marca to set
+	 */
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	/**
+	 * @return the placa
+	 */
+	public String getPlaca() {
+		return placa;
+	}
+
+	/**
+	 * @param placa the placa to set
+	 */
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
